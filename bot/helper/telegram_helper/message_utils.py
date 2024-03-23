83f99ee23c08ecb646241ab6328ebf25a8a8e343
Hrishikesh <hrishithombare15@gmail.com>
2024-03-23 15:12:32 +0530

    ADDED STICKERS AND MODIFIED BY @hrishikesh2861

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -281,7 +281,7 @@ async def sendStatusMessage(msg):
                 message.text = progress
         status_reply_dict[chat_id] = [message, time()]
         if not Interval:
-            Interval.append(setInterval(config_dict['STATUS_UPDATE_INTERVAL'], update_all_messages))
+            Interval.append(setInterval(1, update_all_messages))
 
 
 async def forcesub(message, ids, button=None):
