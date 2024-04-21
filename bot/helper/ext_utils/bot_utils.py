c8fd223aa2ab8f30e2a1591e4943f34dfb349bb6
Hrishikesh <hrishithombare15@gmail.com>
2024-04-21 21:38:17 +0530

    New Look

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -171,7 +171,7 @@ def get_readable_message():
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
         msg += f"<b>\nFile Name</b> » <i>{escape(f'{download.name()}')}</i>\n"
-        msg += f"by <i>{download.message.from_user.mention}</i> | Id: <i>{download.message.from_user.id}</i>\n\n"
+        msg += f"by <code>{download.message.from_user.mention}</code> | Id: <code>{download.message.from_user.id}</code>\n\n"
         msg += f"<b>{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<code>{progress_bar(download.progress())}</code> {download.progress()}"
