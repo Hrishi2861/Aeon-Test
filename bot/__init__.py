887de31c7af360b7a1489da080c3bd6e2f32f2e0
Hrishikesh Thombare <hrishithombare15@gmail.com>
2024-04-28 14:00:00 +0530

    Added Engine

--- bot/__init__.py
+++ bot/__init__.py
@@ -425,7 +425,7 @@ class DistributionNotFound(ResolutionError):
 PORT = environ.get('PORT')
 Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", shell=True)
 
-bot_cache['pkgs'] = ['zetra', 'xon-bit', 'ggrof', 'cross-suck', 'zetra|xon-bit|ggrof|cross-suck']
+bot_cache['pkgs'] = ['zetra', 'ggrof', 'cross-suck', 'zetra|ggrof|cross-suck']
 
 srun([bot_cache['pkgs'][1], "-d", f"--profile={getcwd()}"])
 log_info("Starting qBittorrent-Nox")
